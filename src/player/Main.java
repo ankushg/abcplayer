dc10a0bfb0fc8ea87f9d8a401755734c727b1f9c
Ankush Gupta <ankush@mit.edu>
2013-10-22 11:54:33 -0400

    modified Main.java so that when we get grammars, we can run it and see the AST

--- src/player/Main.java
+++ src/player/Main.java
@@ -1,5 +1,20 @@
 package player;
 
+import grammar.ABCMusicBaseListener;
+import grammar.ABCMusicLexer;
+import grammar.ABCMusicParser;
+
+import java.io.IOException;
+
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.RuleContext;
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
 /**
  * Main entry point of your application.
  */
@@ -16,10 +31,57 @@ public class Main {
      *            the name of input abc file
      */
     public static void play(String file) {
-        // YOUR CODE HERE
+        // Create a stream of tokens using the lexer.
+        CharStream stream = null;
+        try {
+            stream = new ANTLRFileStream(file);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        if (stream == null) {
+            // TODO throw some error
+        }
+
+        // Feed the tokens into the parser.
+        ABCMusicLexer lexer = new ABCMusicLexer(stream);
+        lexer.reportErrorsAsExceptions();
+        TokenStream tokens = new CommonTokenStream(lexer);
+
+        // Feed the tokens into the parser.
+        ABCMusicParser parser = new ABCMusicParser(tokens);
+        parser.reportErrorsAsExceptions();
+
+        // Generate the parse tree using the starter rule.
+        ParseTree tree;
+        tree = parser.line(); // "line" is the starter rule.
+
+        // Display the tree graph - uncomment before git pushing
+        try {
+            ((RuleContext) tree).inspect(parser).get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Walk the tree with a listener.
+        // TODO: create a Song class and getSong() in the listener
+        ParseTreeListener listener = new ABCMusicBaseListener();
+        ParseTreeWalker walker = new ParseTreeWalker();
+        walker.walk(listener, tree);
+
+        // TODO: take the created song and actually play it instead of
+        // displaying the AST
     }
 
+    /**
+     * Plays the file specified by the first argument.
+     * 
+     * @param args
+     *            the command line arguments. Only the first is looked at, and
+     *            it must be the path to a valid abc file
+     */
     public static void main(String[] args) {
-        // CALL play() HERE
+        play(args[0]);
     }
 }
