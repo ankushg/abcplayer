76dd138d767771c712a9d201da9173a8abaafcae
Anand Oza <anand.oza@gmail.com>
2013-10-30 10:05:05 -0400

    formatted tests/package-info.java

--- src/tests/package-info.java
+++ src/tests/package-info.java
@@ -1,4 +1,7 @@
 /**
- * Package containing all of our test files. Our testing strategy is to partition the spaces in the test for each class, but make our objects within those partitions public and static. This allows us to reuse them in tests for components higher in the hierarchy.
+ * Package containing all of our test files. Our testing strategy is to
+ * partition the spaces in the test for each class, but make our objects within
+ * those partitions public and static. This allows us to reuse them in tests for
+ * components higher in the hierarchy.
  */
 package tests;
