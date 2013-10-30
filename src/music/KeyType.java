f2242220c1208fc979b25ee9234b6df885a3cdba
Harini Kannan <hkannan@mit.edu>
2013-10-30 12:37:54 -0400

    Added song fields and fixed key signature bug

--- src/music/KeyType.java
+++ src/music/KeyType.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
  */
 public enum KeyType {
     C(0), G(1), D(2), A(3), E(4), B(5), FS(6), CS(7), F(-1), BF(-2), EF(-3), AF(-4), DF(-5), GF(-6), CF(-7), am(0), em(
-            1), bm(2), fsm(3), csm(4), gsm(5), dsm(6), asm(7), d(1), bfm(2), efm(3), afm(4), dfm(5), gfm(6), cfm(7);
+            1), bm(2), fsm(3), csm(4), gsm(5), dsm(6), asm(7), dm(-1), gm(-2), cm(-3), fm(-4), bfm(-5), efm(-6), afm(-7);
 
     private final HashMap<Character, Integer> map;
 
