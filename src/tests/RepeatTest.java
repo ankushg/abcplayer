b5266333f82a287f70fe083c62f74c02e673233d
Harini Kannan <hkannan@mit.edu>
2013-10-30 16:08:37 -0400

    Added Repeat Tests. Currently failing.

--- src/tests/RepeatTest.java
+++ src/tests/RepeatTest.java
@@ -1,8 +1,24 @@
 package tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import music.Chord;
+import music.ChordSequence;
+import music.ChordSequenceList;
+import music.Fraction;
+import music.KeySignature;
+import music.KeyType;
+import music.Measure;
+import music.Note;
+import music.Repeat;
 
 import org.junit.Test;
+
+import sound.Pitch;
 
 public class RepeatTest {
 
@@ -13,7 +29,97 @@ public class RepeatTest {
 
     @Test
     public void testGetChords() {
-        fail("Not yet implemented"); // TODO test repeat getchords
+
+        Chord c1 = new Chord(FractionTest.oneFourth, new Note(PitchTest.middleC, FractionTest.oneFourth), new Note(
+                PitchTest.lowG, FractionTest.oneFourth));
+        Chord c2 = new Chord(FractionTest.oneHalf, new Note(PitchTest.middleC, FractionTest.oneHalf), new Note(
+                PitchTest.highC, FractionTest.oneFourth));
+        Chord c3 = new Chord(FractionTest.oneFourth, new Note(PitchTest.highC, FractionTest.oneFourth), new Note(
+                PitchTest.lowG, FractionTest.oneFourth));
+
+        Measure m1 = new Measure(c1, c2, c3);
+
+        List<ChordSequence> noKeyCS = new ArrayList<>();
+        noKeyCS.add(new Chord(new Fraction(2, 1), new Note(new Pitch('D'), new Fraction(2, 1))));
+        noKeyCS.add(new Chord(new Fraction(2, 1), new Note(new Pitch('E'), new Fraction(2, 1))));
+        noKeyCS.add(new Chord(new Fraction(2, 1), new Note(new Pitch('F'), new Fraction(2, 1))));
+        noKeyCS.add(new Chord(new Fraction(2, 1), new Note(new Pitch('G'), new Fraction(2, 1))));
+
+        Measure m2 = new Measure(new KeySignature(KeyType.D), noKeyCS);
+
+        List<ChordSequence> firstEnding = new ArrayList<ChordSequence>();
+        firstEnding.add(m2);
+        Repeat r = new Repeat(m1, firstEnding);
+
+        List<Chord> expected = new ArrayList<Chord>();
+        expected.add(c1);
+        expected.add(c2);
+        expected.add(c3);
+        expected.add(new Chord(new Fraction(2, 1), new Note(new Pitch('D'), new Fraction(2, 1))));
+        expected.add(new Chord(new Fraction(2, 1), new Note(new Pitch('E'), new Fraction(2, 1))));
+        expected.add(new Chord(new Fraction(2, 1), new Note(new Pitch('F'), new Fraction(2, 1))));
+        expected.add(new Chord(new Fraction(2, 1), new Note(new Pitch('G'), new Fraction(2, 1))));
+        expected.add(c1);
+        expected.add(c2);
+        expected.add(c3);
+
+        assertEquals(expected, r.getChords());
+
+    }
+
+    @Test
+    public void testGetChordsLongCommon() {
+
+        Chord c1 = new Chord(FractionTest.oneFourth, new Note(PitchTest.middleC, FractionTest.oneFourth), new Note(
+                PitchTest.lowG, FractionTest.oneFourth));
+        Chord c2 = new Chord(FractionTest.oneHalf, new Note(PitchTest.middleC, FractionTest.oneHalf), new Note(
+                PitchTest.highC, FractionTest.oneFourth));
+        Chord c3 = new Chord(FractionTest.oneFourth, new Note(PitchTest.highC, FractionTest.oneFourth), new Note(
+                PitchTest.lowG, FractionTest.oneFourth));
+
+        Measure m1 = new Measure(c1, c2, c3);
+        Measure m2 = new Measure(c2, c3, c1);
+        Measure m3 = new Measure(c3, c1, c2);
+        ChordSequenceList common = new ChordSequenceList(m1, m2, m3);
+
+        List<ChordSequence> noKeyCS = new ArrayList<>();
+        noKeyCS.add(new Chord(new Fraction(2, 1), new Note(new Pitch('D'), new Fraction(2, 1))));
+        noKeyCS.add(new Chord(new Fraction(2, 1), new Note(new Pitch('E'), new Fraction(2, 1))));
+        noKeyCS.add(new Chord(new Fraction(2, 1), new Note(new Pitch('F'), new Fraction(2, 1))));
+        noKeyCS.add(new Chord(new Fraction(2, 1), new Note(new Pitch('G'), new Fraction(2, 1))));
+
+        Measure m4 = new Measure(new KeySignature(KeyType.D), noKeyCS);
+
+        List<ChordSequence> firstEnding = new ArrayList<ChordSequence>();
+        firstEnding.add(m4);
+        Repeat r = new Repeat(common, firstEnding);
+
+        List<Chord> expected = new ArrayList<Chord>();
+        expected.add(c1);
+        expected.add(c2);
+        expected.add(c3);
+        expected.add(c2);
+        expected.add(c3);
+        expected.add(c1);
+        expected.add(c3);
+        expected.add(c1);
+        expected.add(c2);
+        expected.add(new Chord(new Fraction(2, 1), new Note(new Pitch('D'), new Fraction(2, 1))));
+        expected.add(new Chord(new Fraction(2, 1), new Note(new Pitch('E'), new Fraction(2, 1))));
+        expected.add(new Chord(new Fraction(2, 1), new Note(new Pitch('F'), new Fraction(2, 1))));
+        expected.add(new Chord(new Fraction(2, 1), new Note(new Pitch('G'), new Fraction(2, 1))));
+        expected.add(c1);
+        expected.add(c2);
+        expected.add(c3);
+        expected.add(c2);
+        expected.add(c3);
+        expected.add(c1);
+        expected.add(c3);
+        expected.add(c1);
+        expected.add(c2);
+
+        assertEquals(expected, r.getChords());
+
     }
 
     @Test
